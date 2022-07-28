160422590d388cb1edd3bf0fc341da09b8ea7554
Ivan Ruiz <ivanrur95@hotmail.com>
2022-07-28 11:06:44 -0500

    se hace commit inicial nuevo repo

---
+++ img/index.js
@@ -0,0 +1,31 @@
+import logo from './logo.png';
+import videoBanner from '../video/banner.mp4';
+import footer from './footer.png';
+import logoGajes from './logoGajes.png';
+import banner1 from './banner-1.jpg';
+import banner2 from './banner-2.jpg';
+import banner3 from './banner-3.jpg';
+import banner4 from './banner-4.jpg';
+import banner5 from './banner-5.jpg';
+import videoVisionMX from '../video/visionmx_banner.mp4';
+
+// STAFF
+import staffBrenda from './staff_brenda.png'
+
+
+const img = {
+    videoVisionMX,
+    logo,
+    videoBanner,
+    footer,
+    logoGajes,
+    banner1,
+    banner2,
+    banner3,
+    banner4,
+    banner5,
+    staffBrenda
+
+}
+
+export default img
